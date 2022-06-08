cb281da61e4fbcd1566244c36deeb2343f255a8b
NikitaBarashkov <nik96barashkov@yandex.by>
2022-06-08 17:39:39 +0300

    feat: add all content in body(Wed, 08 June 2022, 17:39)

--- index.js
+++ index.js
@@ -13,3 +13,39 @@ const namePage = document.createElement('h1');
 fixBlockHead.prepend(containerNamePage);
 containerNamePage.prepend(namePage);
 namePage.innerHTML = `Junior Front-end develoter`;
+
+const button = document.querySelectorAll('.nav-list_button');
+
+
+// function showAboutMe(){
+//     console.log('Yes');
+// }
+// const content = document.querySelector('.right-block__container');
+// const aboutMe = document.createElement('div');
+// aboutMe.innerHTML = `I am some man who was born in Belarus`;
+
+
+// button.forEach(itemBut => {
+//   itemBut.addEventListener('click', (event)=>{
+//     if(event.target.classList.contains('one')){
+//        content.append(aboutMe);
+//     } else if(event.target.classList.contains('two')){
+//         content.remove(aboutMe);  
+//     } else if(event.target.classList.contains('tree')){
+//         console.log('Third button');    
+//     } else if(event.target.classList.contains('four')){
+//         console.log('Sond button');    
+//     } else if(event.target.classList.contains('five')){
+//         console.log('S button');    
+//     } else if(event.target.classList.contains('six')){
+//         console.log('d button');    
+//     }
+    
+//   })
+// })
+
+const me = {
+    name: 'Nikita',
+    age: 25,
+  }
+  console.log(me);
